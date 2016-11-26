84f6d5e69cfea8479394e38c13164be6650abf68
ScriptSmith <ScriptSmith@users.noreply.github.com>
2016-11-26 15:24:25 +1000

    added suburb data

---
+++ data.js
@@ -0,0 +1,249 @@
+// Guide:
+// 1 is bad, 10 is good (except where specified in brackets)
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// median_housing_cost- The median cost of a house based in the suburb (1 is cheap, 10 is expensive)
+// traffic- The rating given to time taken to travel, congestion etc. (1 is little, 10 is lots)
+// poi- The rating given to the amount of interesting points of interest in the suburb
+// construction- The rating given to the amount of construction ocurring in the area
+// noise- The rating given to the complaints by neighbours about noise
+// public_transport- The rating given to public transport options in the area
+// distance_to_cbd- The distance to the center of brisbane (1 is far, 10 is near)
+
+var suburbs = {
+    bracken_ridge: {
+        median_housing_cost: 4,
+        traffic: 8,
+        poi: 3,
+        construction: 2,
+        noise: 9,
+        public_transport: 8,
+        distance_to_cbd: 2
+    },
+    calamvale: {
+        median_housing_cost: 7,
+        traffic: 7,
+        poi:4,
+        construction: 1,
+        noise: 2,
+        public_transport: 6,
+        distance_to_cbd: 4
+    },
+    central: {
+        median_housing_cost: 10,
+        traffic: 9,
+        poi: 10,
+        construction: 10,
+        noise: 1,
+        public_transport: 10,
+        distance_to_cbd: 10
+    },
+    chandler: {
+        median_housing_cost: 7,
+        traffic: 4,
+        poi: 4,
+        construction: 8,
+        noise: 2,
+        public_transport: 7,
+        distance_to_cbd: 5
+    },
+    coorparoo: {
+        median_housing_cost: 5,
+        traffic: 10,
+        poi: 4,
+        construction: 10,
+        noise: 7,
+        public_transport: 9,
+        distance_to_cbd: 8
+    },
+    deagon: {
+        median_housing_cost: 8,
+        traffic: 3,
+        poi: 4,
+        construction: 1,
+        noise: 10,
+        public_transport: 4,
+        distance_to_cbd: 3
+    },
+    doboy: {
+        median_housing_cost: 8,
+        traffic: 4,
+        poi: 7,
+        construction: 10,
+        noise: 8,
+        public_transport: 7,
+        distance_to_cbd: 7
+    },
+    enoggera: {
+        median_housing_cost: 6,
+        traffic: 8,
+        poi: 3,
+        construction: 10,
+        noise: 4,
+        public_transport: 8,
+        distance_to_cbd: 8
+    },
+    forest_lake: {
+        median_housing_cost: 2,
+        traffic: 7,
+        poi: 6,
+        construction: 6,
+        noise: 8,
+        public_transport: 5,
+        distance_to_cbd: 4
+    },
+    hamilton: {
+        median_housing_cost: 9,
+        traffic: 8,
+        poi: 9,
+        construction: 4,
+        noise: 6,
+        public_transport: 9,
+        distance_to_cbd: 10
+    },
+    holland_park: {
+        median_housing_cost: 4,
+        traffic: 7,
+        poi: 3,
+        construction: 6,
+        noise: 7,
+        public_transport: 8,
+        distance_to_cbd: 5
+    },
+    jamboree: {
+        median_housing_cost: 4,
+        traffic: 9,
+        poi: 1,
+        construction: 7,
+        noise: 5,
+        public_transport: 3,
+        distance_to_cbd: 1
+    },
+    mcdowall: {
+        median_housing_cost: 3,
+        traffic: 6,
+        poi: 2,
+        construction: 7,
+        noise: 4,
+        public_transport: 7,
+        distance_to_cbd: 3
+    },
+    macgregor: {
+        median_housing_cost: 2,
+        traffic: 7,
+        poi: 4,
+        construction: 8,
+        noise: 5,
+        public_transport: 7,
+        distance_to_cbd: 3
+    },
+    marchant: {
+        median_housing_cost: 7,
+        traffic: 9,
+        poi: 8,
+        construction: 5,
+        noise: 2,
+        public_transport: 8,
+        distance_to_cbd: 8
+    },
+    moorooka: {
+        median_housing_cost: 3,
+        traffic: 7,
+        poi: 4,
+        construction: 7,
+        noise: 4,
+        public_transport: 7,
+        distance_to_cbd: 8
+    },
+    morningside: {
+        median_housing_cost: 8,
+        traffic: 9,
+        poi:7,
+        construction: 5,
+        noise: 8,
+        public_transport: 8,
+        distance_to_cbd: 8
+    },
+    northgate: {
+        median_housing_cost: 3,
+        traffic: 4,
+        poi: 2,
+        construction: 8,
+        noise: 8,
+        public_transport: 7,
+        distance_to_cbd: 2
+    },
+    paddington: {
+        median_housing_cost: 8,
+        traffic: 8,
+        poi: 9,
+        construction: 10,
+        noise: 4,
+        public_transport: 10,
+        distance_to_cbd: 9
+    },
+    pullenvale: {
+        median_housing_cost: 1,
+        traffic: 1,
+        poi: 3,
+        construction: 9,
+        noise: 3,
+        public_transport: 0,
+        distance_to_cbd: 10
+    },
+    runcorn: {
+        median_housing_cost: 1,
+        traffic: 2,
+        poi: 5,
+        construction: 3,
+        noise: 4,
+        public_transport: 7,
+        distance_to_cbd: 1
+    },
+    tennyson: {
+        median_housing_cost: 2,
+        traffic: 5,
+        poi: 6,
+        construction: 0,
+        noise: 3,
+        public_transport: 2,
+        distance_to_cbd: 2
+    },
+    the_gabba: {
+        median_housing_cost: 9,
+        traffic: 9,
+        poi: 9,
+        construction: 7,
+        noise: 10,
+        public_transport: 10,
+        distance_to_cbd: 9
+    },
+    the_gap: {
+        median_housing_cost: 4,
+        traffic: 6,
+        poi: 6,
+        construction: 3,
+        noise: 5,
+        public_transport: 8,
+        distance_to_cbd: 6
+    },
+    walter_taylor: {
+        median_housing_cost: 7,
+        traffic: 5,
+        poi: 5,
+        construction: 4,
+        noise: 8,
+        public_transport: 3,
+        distance_to_cbd: 5
+    },
+    wynnum_manly: {
+        median_housing_cost: 5,
+        traffic: 3,
+        poi: 6,
+        construction: 2,
+        noise: 2,
+        public_transport: 2,
+        distance_to_cbd: 1
+    }
+}
