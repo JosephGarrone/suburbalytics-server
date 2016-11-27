7d17e47e8b8f414df294a113648b2ca142c97cc1
JosephGarrone <josephgarrone@gmail.com>
2016-11-27 11:04:25 +1000

    Latest

--- public/js/page.js
+++ public/js/page.js
@@ -1,5 +1,15 @@
-var textData = ["#housing", "#traffic", "#poi", "#construction", "#noise", "#transport", "distance"];
-var scale = ["Very poor", "Poor", "Average", "Good", "Very good"]
+var textData = [
+    ["median_housing_cost", "#housing"], 
+    ["traffic", "#traffic"], 
+    ["poi", "#poi"], 
+    ["construction", "#constructions"], 
+    ["noise", "#noise"], 
+    ["public_transport", "#transport"], 
+    ["distance_to_cbd", "#distance"]
+];
+var scale = ["Very poor", "Poor", "Average", "Good", "Very good"];
+var colors = ["red", "orange", "black", "green", "darkgreen"];
+var data = null;
 
 function runJS() {
     $('#go').click(() => {
@@ -11,7 +21,14 @@ function runJS() {
                 $('#sample1').val('');
                 $('#sample1').val('');
 
-                loadInfo();
+                $("#searchTerm").text($("#search").val());
+
+                loadInfo($('#search').val().toLowerCase().replace(' ', '_'));
+
+                $('#search').on('keyup', () => {
+                    $("#searchTerm").text($("#search").val());
+                    loadInfo($('#search').val().toLowerCase().replace(' ', '_'));
+                })
             }});
         }});
     });
@@ -22,12 +39,20 @@ function runJS() {
     });
 }
 
-function loadInfo() {
-    for (var i = 0; i < textData.length; i++) {
-        (function(index) {
-            $(sliders[index]).on("change", (value, event) => {
-                $(sliders[index] + "_value").text($(sliders[index]).val());
-            });
-        })(i);
-    }
+function loadInfo(suburb) {
+    $.getJSON('/data/' + suburb, {}, function(data) {
+        console.log(data);
+        if (data.err) {
+            $("#notFound").fadeIn();
+        } else {
+            $("#notFound").fadeOut();
+            for (var i = 0; i < textData.length; i++) {
+                var item = textData[i];
+                var id = item[1];
+                var idx = Math.floor(data[item[0]]/2);
+                $(id).text(scale[idx]);
+                $(id).css({'color': colors[idx]})
+            }
+        }
+    });
 }
